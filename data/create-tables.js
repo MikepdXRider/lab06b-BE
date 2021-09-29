e7009e44674154f75594b44d932782ffd6895a26
Michael Rider <mikepdxrider@gmail.com>
2021-09-29 13:27:31 -0700

    updates source data and create-tables

--- data/create-tables.js
+++ data/create-tables.js
@@ -17,10 +17,12 @@ async function run() {
                     email VARCHAR(256) NOT NULL,
                     hash VARCHAR(512) NOT NULL
                 );           
-                CREATE TABLE animals (
+                CREATE TABLE teas (
                     id SERIAL PRIMARY KEY NOT NULL,
-                    name VARCHAR(512) NOT NULL,
-                    cool_factor INTEGER NOT NULL,
+                    tea VARCHAR(512) NOT NULL,
+                    type VARCHAR(512) NOT NULL,
+                    description VARCHAR(512) NOT NULL,
+                    north_america_native VARCHAR(512) NOT NULL,
                     owner_id INTEGER NOT NULL REFERENCES users(id)
             );
         `);
---
+++ data/data.js
@@ -0,0 +1,37 @@
+module.exports = [
+  {
+    tea: 'Darjeeling',
+    type: 'Black',
+    description: 'Derivative of Black Tea with a light, nutty taste to it and a floral smell.',
+    north_america_native: false,
+    url: 'https://cdn.shopify.com/s/files/1/0415/5182/3016/articles/5e62a4da51beefb68fbc4ae0_AdobeStock_317029222_1024x1024.jpeg?v=1596741272'
+  },
+  {
+    tea: 'English Breakfast',
+    type: 'Black',
+    description: 'Has a rich and hearty flavor and is often enjoyed with milk and sugar.',
+    north_america_native: false,
+    url: 'https://cdnimg.webstaurantstore.com/images/products/large/542790/1993727.jpg'
+  },
+  {
+    tea: 'Matcha',
+    type: 'Green',
+    description: 'Has a rich and hearty flavor and is often enjoyed with milk and sugar.',
+    north_america_native: false,
+    url: 'https://www.nishikidori.com/579-large_default/organic-matcha-tea-from-nishio-aichi-premium-quality.jpg'
+  },
+  {
+    tea: 'Mint',
+    type: 'Green',
+    description: 'Tastes like mint leaves and helps to soothe upset stomachs.',
+    north_america_native: false,
+    url: 'https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/325242_1100-1100x628.jpg'
+  },
+  {
+    tea: 'Chamomile',
+    type: 'herbal',
+    description: 'Is known for its soothing properties with a floral flavoring.',
+    north_america_native: false,
+    url: 'https://post.healthline.com/wp-content/uploads/2020/09/chamomile-tea-thumb-1-732x549.jpg'
+  },
+];

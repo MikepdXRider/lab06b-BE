37648a2761f11be682c0b22bb3d475125c338ed8
Michael Rider <mikepdxrider@gmail.com>
2021-09-29 13:30:09 -0700

    updates drop-tables and load-seed-data

--- data/drop-tables.js
+++ data/drop-tables.js
@@ -10,7 +10,7 @@ async function run() {
     
     await client.query(`
             DROP TABLE IF EXISTS users CASCADE;
-            DROP TABLE IF EXISTS animals;
+            DROP TABLE IF EXISTS teas;
         `);
 
     console.log(' drop tables complete', getEmoji(), getEmoji(), getEmoji());
